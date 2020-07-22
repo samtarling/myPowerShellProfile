a65b9d20b9a55135f3e7483221d9ffc21fcdcd95
Sam Tarling <st@samt.dev>
2020-07-22 16:34:16 +0100

    "touch" command, increase history

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
+# Increase history
+$MaximumHistoryCount = 10000
+
+#Functions
+#> sudo [application/command]
 function sudo {
 	$file, [string]$arguments = $args;
 	$psi = new-object System.Diagnostics.ProcessStartInfo $file;
@@ -5,4 +10,9 @@ function sudo {
 	$psi.Verb = "runas";
 	$psi.WorkingDirectory = get-location;
 	[System.Diagnostics.Process]::Start($psi) >> $null
+}
+
+#> touch [filename]
+function touch($file) {
+	"" | Out-File $file -Encoding ASCII
 }
